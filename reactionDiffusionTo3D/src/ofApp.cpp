2abd7f96001017cc33c70cd346f2acacfae3ea45
edapx <lastexxit@gmail.com>
2016-11-16 09:03:52 +0100

    disable normals shader

--- reactionDiffusionTo3D/src/ofApp.cpp
+++ reactionDiffusionTo3D/src/ofApp.cpp
@@ -29,7 +29,7 @@ void ofApp::setup(){
     } else {
         //shader.load(shadersFolder+"/passthru.vert", shadersFolder+"/grayscott.frag");
         shader.load(shadersFolder+"/passthru.vert", shadersFolder+"/grayscott.frag");
-        updateRender.load(shadersFolder+"/normals.vert", shadersFolder+"/normals.frag");
+        //updateRender.load(shadersFolder+"/normals.vert", shadersFolder+"/normals.frag");
         //updateRender.load(shadersFolder+"/displacement.vert", shadersFolder+"/render.frag");
     };
     clearBuffersAndAllocate();
@@ -69,11 +69,11 @@ void ofApp::draw(){
     cam.begin();
     texture.bind();
     plane.mapTexCoordsFromTexture(texture);
-    updateRender.begin();
-    updateRender.setUniform1f("discardRed", discardRed);
-    updateRender.setUniform1f("displaceAmount", displaceAmount);
+    //updateRender.begin();
+    //updateRender.setUniform1f("discardRed", discardRed);
+    //updateRender.setUniform1f("displaceAmount", displaceAmount);
     plane.draw();
-    updateRender.end();
+    //updateRender.end();
     texture.unbind();
     cam.end();
     ofDisableDepthTest();
